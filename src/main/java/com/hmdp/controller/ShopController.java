2d5437f6239bb96d0e5631cac4dc7221e771b2c1
BeAWitch <109846146+BeAWitch@users.noreply.github.com>
2025-07-12 15:19:51 +0800

    附近店铺、用户签到

--- src/main/java/com/hmdp/controller/ShopController.java
+++ src/main/java/com/hmdp/controller/ShopController.java
@@ -97,4 +97,22 @@ public class ShopController {
         // 返回数据
         return Result.ok(page.getRecords());
     }
+
+    /**
+     * 根据商铺类型 id 查询商铺信息
+     * @param typeId 商铺类型 id
+     * @param current 页码
+     * @param x 经度
+     * @param y 维度
+     * @return 商铺列表
+     */
+    @GetMapping("/of/type")
+    public Result queryShopByType(
+            @RequestParam("typeId") Integer typeId,
+            @RequestParam(value = "current", defaultValue = "1") Integer current,
+            @RequestParam(value = "x", required = false) Double x,
+            @RequestParam(value = "y", required = false) Double y
+    ) {
+        return shopService.queryShopByType(typeId, current, x, y);
+    }
 }
